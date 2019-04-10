4eb248577afeabe951b210fd89e5c2a3ca7bc226
marnuncas1 <martanunezcascales@gmail.com>
2019-04-10 12:20:29 +0200

    2.primera versión de las tablas1

---
+++ database/migrations/2019_04_10_082018_create_trabajadores_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTrabajadoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employee', function (Blueprint $table) {
+            $table->bigIncrements('id_employee');
+            $table->string('name');
+            $table->string('surname1');
+            $table->string('surname2');
+            $table->string('telephone');
+            $table->string('address');
+            $table->string('cp');
+            $table->string('password')->unique();
+            $table->timestamp('password_verified_at')->nullable();
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('nif');
+            $table->string('nationality');
+            $table->string('nie');
+            $table->string('post');
+            $table->rememberToken();
+            $table->timestamps();
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trabajadores');
+    }
+}
