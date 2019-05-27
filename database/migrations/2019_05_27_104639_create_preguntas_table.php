89ad7b5ae3ed69284c3531bfde45a1a3e18b82aa
aroa1939 <aroa.l.a.97@gmail.com>
2019-05-27 13:56:45 +0200

    version 2 con la correcta creacion de la BD y nombres en mismo idioma.

---
+++ database/migrations/2019_05_27_104639_create_preguntas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePreguntasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('preguntas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('tituloPregunta');
+            $table->unsignedBigInteger('encuesta_id');
+            $table->timestamps();
+
+            $table->foreign('encuesta_id')->references('id')->on('encuestas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('preguntas');
+    }
+}
---
+++ database/migrations/2019_05_27_104713_create_respuestas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRespuestasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('respuestas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('respuesta');
+
+            $table->unsignedBigInteger('pregunta_id');
+            $table->timestamps();
+
+            $table->foreign('pregunta_id')->references('id')->on('preguntas')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('respuestas');
+    }
+}
